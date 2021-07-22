b81171aeedfcdd2b73cc7d851876c20446b9c8e9
rromaniz <romaniz.avila@gmail.com>
2021-07-22 00:14:20 -0500

    Se agrego Hero animation

--- lib/widgets/movie_slider.dart
+++ lib/widgets/movie_slider.dart
@@ -58,8 +58,9 @@ class _MovieSliderState extends State<MovieSlider> {
                 controller: scrollController,
                 scrollDirection: Axis.horizontal,
                 itemCount: widget.movies.length,
-                itemBuilder: (_, int index) =>
-                    _MoviePoster(widget.movies[index])),
+                itemBuilder: (_, int index) => _MoviePoster(
+                    widget.movies[index],
+                    '${widget.title}-$index-${widget.movies[index].id}')),
           )
         ],
       ),
@@ -69,9 +70,13 @@ class _MovieSliderState extends State<MovieSlider> {
 
 class _MoviePoster extends StatelessWidget {
   final Movie movie;
-  const _MoviePoster(this.movie);
+  final String heroId;
+
+  const _MoviePoster(this.movie, this.heroId);
   @override
   Widget build(BuildContext context) {
+    movie.heroId = heroId;
+
     return Container(
       width: 130,
       height: 190,
@@ -81,14 +86,17 @@ class _MoviePoster extends StatelessWidget {
           GestureDetector(
             onTap: () =>
                 Navigator.pushNamed(context, 'details', arguments: movie),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: FadeInImage(
-                placeholder: AssetImage('assets/no-image.jpg'),
-                image: NetworkImage(movie.fullPosterImg),
-                width: 130,
-                height: 180,
-                fit: BoxFit.cover,
+            child: Hero(
+              tag: movie.heroId!,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: FadeInImage(
+                  placeholder: AssetImage('assets/no-image.jpg'),
+                  image: NetworkImage(movie.fullPosterImg),
+                  width: 130,
+                  height: 180,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
           ),
